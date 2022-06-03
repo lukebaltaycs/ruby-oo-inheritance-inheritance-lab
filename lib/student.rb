6b35666c817009cfaf1b8aa5246b90b9658eef6a
Luke Baltay <luke.baltay@yale.edu>
2022-06-03 12:38:09 -0400

    commit tree 7c49ef7f069c832c07ca5ff5986dc6f690049d02

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(info)
+        self.knowledge.append(info)
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
